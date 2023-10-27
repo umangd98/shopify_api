52aad8dc234ea691dca3551687bdf3f93d855105
umangd98 <umangd98@gmail.com>
2023-10-27 16:31:47 +0530

    init

---
+++ constants.py
@@ -0,0 +1,8 @@
+APIKEY = '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'
+
+brwoserless_api_key = '4b393b36-1c83-455f-81f7-94b66089c7a4'
+serper_api_key = '2ba532124098115ebb56d3ee4276f75e1fffa32e'
+
+serp_api_key = '372c867f1f66452b2ae726459a1e3fdccf958d81ac25af371537871a9f31a2c6'
+
+zapier_api_key = 'sk-ak-vkVXkKUCoS5yJBT0E7yLsF4AS9'
